f449f331c7840829c17f84d3820ca5ffe5183f95
Sasabmeg <eeeeeeediot@gmail.com>
2022-09-01 18:19:19 +0200

    Added template to show joke. Closes #4.

--- src/main/java/sasa/springframework/chucknorrisjokes/services/JokeServiceImpl.java
+++ src/main/java/sasa/springframework/chucknorrisjokes/services/JokeServiceImpl.java
@@ -2,10 +2,16 @@ package sasa.springframework.chucknorrisjokes.services;
 
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 @Service
 public class JokeServiceImpl implements JokeService {
+    private final String[] jokes = {"Chuck Norris counted to infinity... twice!",
+                                    "Did you know aliens exist? They just afraid to invade earth until Chuck Norris lives.",
+                                    "Chuck Norris had 3 enemies... may they rest in peace."};
+
     @Override
     public String getJoke() {
-        return "Chuck Norris counted to infinity... twice!";
+        return jokes[ThreadLocalRandom.current().nextInt(0, 3)];
     }
 }

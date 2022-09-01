f3490c3a45e54d5ce1aed52673a80e7b898e306f
Sasabmeg <eeeeeeediot@gmail.com>
2022-09-01 17:32:55 +0200

    Generated and added spring-web and thymeleaf to project.

---
+++ src/test/java/sasa/springframework/chucknorrisjokes/ChuckNorrisJokesApplicationTests.java
@@ -0,0 +1,13 @@
+package sasa.springframework.chucknorrisjokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckNorrisJokesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
